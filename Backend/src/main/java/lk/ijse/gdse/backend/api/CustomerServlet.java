bcceccf34ab2ac15c58d661dd87fb2dc8f49e7b3
YurajMalinda <yurajmalinda123.com>
2024-02-03 00:00:51 +0530

    Customer Servlet extends with HttpServlet and Http methods overrides to Customer Servlet.

--- Backend/src/main/java/lk/ijse/gdse/backend/api/CustomerServlet.java
+++ Backend/src/main/java/lk/ijse/gdse/backend/api/CustomerServlet.java
@@ -1,4 +1,36 @@
 package lk.ijse.gdse.backend.api;
 
-public class CustomerServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "customerServlet", urlPatterns = "/customers")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
 }
