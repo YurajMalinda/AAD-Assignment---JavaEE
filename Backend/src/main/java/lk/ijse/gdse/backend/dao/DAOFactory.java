a26880e9efa2f1587a5483a5bb8f28236f8ac1cb
YurajMalinda <yurajmalinda123.com>
2024-02-04 19:16:30 +0530

    DAOFactory completed.

--- Backend/src/main/java/lk/ijse/gdse/backend/dao/DAOFactory.java
+++ Backend/src/main/java/lk/ijse/gdse/backend/dao/DAOFactory.java
@@ -1,4 +1,40 @@
 package lk.ijse.gdse.backend.dao;
 
+import lk.ijse.gdse.backend.dao.custom.impl.CustomerDAOImpl;
+import lk.ijse.gdse.backend.dao.custom.impl.ItemDAOImpl;
+import lk.ijse.gdse.backend.dao.custom.impl.OrderDAOImpl;
+import lk.ijse.gdse.backend.dao.custom.impl.OrderDetailsDAOImpl;
+
 public class DAOFactory {
+    private static DAOFactory daoFactory;
+
+    public DAOFactory() {
+
+    }
+
+    public static DAOFactory getDaoFactory(){
+        return (daoFactory == null)? daoFactory = new DAOFactory() : daoFactory;
+    }
+
+    public enum DAOTypes{
+        CUSTOMER_DAO,
+        ITEM_DAO,
+        ORDERS_DAO,
+        ORDER_DETAILS_DAO
+    }
+
+    public <T extends SuperDAO> T getDao(DAOTypes daoTypes){
+        switch (daoTypes){
+            case CUSTOMER_DAO:
+                return (T) new CustomerDAOImpl();
+            case ITEM_DAO:
+                return (T) new ItemDAOImpl();
+            case ORDERS_DAO:
+                return (T) new OrderDAOImpl();
+            case ORDER_DETAILS_DAO:
+                return (T) new OrderDetailsDAOImpl();
+            default:
+                return null;
+        }
+    }
 }
