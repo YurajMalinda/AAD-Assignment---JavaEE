9d6502350d07b72b70f8844c6a8e725a04534f52
YurajMalinda <yurajmalinda123.com>
2024-02-04 18:59:10 +0530

    DAO package completed.

--- Backend/src/main/java/lk/ijse/gdse/backend/dto/ItemDTO.java
+++ Backend/src/main/java/lk/ijse/gdse/backend/dto/ItemDTO.java
@@ -1,4 +1,15 @@
 package lk.ijse.gdse.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemDTO {
+    private String itemCode;
+    private String itemName;
+    private double price;
+    private int qty;
 }
--- Backend/src/main/java/lk/ijse/gdse/backend/dto/OrderDTO.java
+++ Backend/src/main/java/lk/ijse/gdse/backend/dto/OrderDTO.java
@@ -1,4 +1,18 @@
 package lk.ijse.gdse.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderDTO {
+    private String orderId;
+    private String orderDate;
+    private String custId;
+    private double total;
+    private List<OrderDetailDTO> orderDetailList;
 }
